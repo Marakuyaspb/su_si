5178924e59f6f8adea76bae6ee58ad9fca9d5eac
Marakuyaspb <marakuyaspb@gmail.com>
2024-02-24 15:26:47 +0300

    clean all

--- static/js/calc.js
+++ static/js/calc.js
@@ -6,129 +6,117 @@ document.querySelectorAll('.only_int').forEach(
       });
 });
 
+
 /* FAST VALUES - DEPOSITS */
-document.getElementById('th3').addEventListener('click', function() {
-  document.getElementById('deposit').value = '3000';
+document.getElementById('th3_mob').addEventListener('click', function() {
+  document.getElementById('deposit_mob').value = '3000';
 })
-document.getElementById('th10').addEventListener('click', function() {
-  document.getElementById('deposit').value = '10000';
+document.getElementById('th10_mob').addEventListener('click', function() {
+  document.getElementById('deposit_mob').value = '10000';
 })
-document.getElementById('th50').addEventListener('click', function() {
-  document.getElementById('deposit').value = '50000';
+document.getElementById('th50_mob').addEventListener('click', function() {
+  document.getElementById('deposit_mob').value = '50000';
 })
-
-
-/* FAST VALUES - MONTHS */
-document.getElementById('m1').addEventListener('click', function() {
-  document.getElementById('mths').value = '12';
-});
-
-document.getElementById('m3').addEventListener('click', function() {
-  document.getElementById('mths').value = '24';
-});
-
-document.getElementById('m6').addEventListener('click', function() {
-  document.getElementById('mths').value = '36';
-});
-
 
 
 function showResult(){
+
   /* delete old notions */
-  document.getElementById('notion_deposit').innerText = '';
-  document.getElementById('notion').innerText = '';
+  document.getElementById('notion_deposit_mob').innerText = '';
+  document.getElementById('notion_apr_mob').innerText = '';
+  document.getElementById('result_inner_mob').innerText = '';
+  document.getElementById('deposit_mob').classList.remove('blue_tg_bg_op');
+  document.getElementById('show_result_mob').classList.remove('disabled');
+  /* */
 
-
-
-  
-
-
-  let deposit = parseInt(document.getElementById('deposit').value); 
+  let deposit = parseInt(document.getElementById('deposit_mob').value); 
 
   let rate;
 
   if (deposit === 0 || deposit === null || isNaN(deposit)){
-      document.getElementById('notion_deposit').innerText = 'Enter any amount, please!';
+      document.getElementById('notion_deposit_mob').innerText = 'Enter any amount, please!';
+      document.getElementById('show_result_mob').classList.add('disabled');
       rate = 0;
-  } else if (deposit <= 10000) {
+  } else if (deposit > 0 && deposit < 500) {
+      document.getElementById('notion_deposit_mob').innerText = 'This is too small amount. Enter value bigger than $500, please';
+      document.getElementById('show_result_mob').classList.add('disabled');
+      rate = 0;
+  }  else if (deposit >= 500 && deposit <= 10000) {
       rate = 0.3;
   } else if (deposit > 10000 && deposit < 50000) {
       rate = 0.25;
-  } else if (deposit >= 50000) {
+  } else if (deposit >= 50000 && deposit < 100000) {
       rate = 0.2;
-  } 
-
-
-  let mths = parseInt(document.getElementById('mths').value);
+  } else if (deposit >= 100000) {
+      document.getElementById('notion_deposit_mob').innerHTML = `
+      <span class=''>Because of the size of the deposit we would like to offer you </span><b><u>special conditions! </u></b>
+      <br><span class=''>Please, <a href='https://t.me/cryptoreaper_06'>contact us <img src='static/icons/telegram.svg' class='icon_xs'></a></span>
+      `;
+      document.getElementById('deposit_mob').classList.add('blue_tg_bg_op');
+      document.getElementById('show_result_mob').classList.add('disabled');
+      rate = 0;
+  }
   
 
   let apr = 0;
   let apr_show = 0;
-  if (document.getElementById('optimist').checked) {
-      apr = parseInt((document.getElementById('optimist').value)/100);
+  if (document.getElementById('optimist_mob').checked) {
+      apr = parseFloat((document.getElementById('optimist_mob').value)/100);
       apr_show = 320;
   } 
-  else if (document.getElementById('realist').checked) {
-      apr = parseInt((document.getElementById('realist').value)/100);
+  else if (document.getElementById('realist_mob').checked) {
+      apr = parseFloat((document.getElementById('realist_mob').value)/100);
       apr_show = 120;
   } 
-  else if (document.getElementById('pessimist').checked) {
-      apr = parseInt((document.getElementById('pessimist').value)/100);
+  else if (document.getElementById('pessimist_mob').checked) {
+      apr = parseFloat((document.getElementById('pessimist_mob').value)/100);
       apr_show = 80;
+  } else{
+    document.getElementById('notion_apr_mob').innerText = 'Choise APR, please!';
+    document.getElementById('show_result_mob').classList.add('disabled');
   }
 
 
   let plan;
-  const checkbox = document.getElementById('subscr_plan');
+  const checkbox = document.getElementById('subscr_plan_mob');
 
   if (checkbox.checked) {
       plan = parseInt(25*12);
   } else {
       plan = parseInt(50*12);
   }
-    console.log(apr);
+
 
   let the_profit;
-
-
-  if (mths < 600) {
-    
+ 
     if (deposit === 0 || deposit === null || isNaN(deposit)) {
-      document.getElementById('notion_deposit').innerText = 'Enter any amount, please!';
+      document.getElementById('notion_deposit_mob').innerText = 'Enter any amount, please!';
+      document.getElementById('show_result_mob').classList.add('disabled');
+    } else if(apr === 0 || apr === null || isNaN(apr)){
+      document.getElementById('notion_apr_mob').innerText = 'Choise APR, please!';
+      document.getElementById('show_result_mob').classList.add('disabled');
     } 
     else {
       let the_profit = 
 
-        deposit + ( ( (deposit * (apr-rate)) ) - plan) * (mths/12)
+         deposit+deposit*apr_show/100-deposit*apr_show/100*rate-plan
 
       ;
 
-  console.log(the_profit);
       let the_profit_floor = Math.floor(the_profit);
 
-      let the_profit_space = the_profit.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+      let the_profit_space = the_profit_floor.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
-      document.getElementById('result_inner').innerHTML = `<div class='calc_result mb-3'>
+      document.getElementById('result_inner_mob').innerHTML = `<div class='calc_result mb-3'>
           <div class='profit_info btn_glass'>
-            <div class='calc_details'>YOUR  PROFIT</div>
+            <div class='calc_details'>TOTAL</div>
             <div id='profit_result' class=''>$ ${the_profit_space}</div>
           </div>
           <div class='mt-2'>
             <div class='calc_details'>Deposit: $ ${deposit}</div>
             <div class='calc_details'>APR: ${apr_show} %</div>
-            <div class='calc_details'>Period: ${mths} months</div>
             <div class='calc_details pb-2'>Subscribtion: $ ${plan}/year </div>
           </div></div>`;
       }
-  }
 
-  else if(mths === 0 || mths === null || isNaN(mths)){
-    document.getElementById('notion').innerText = 'Enter number of months, please';
-  } 
-  else {
-    let years =Math.floor(mths/12)
-
-    document.getElementById('notion').innerText = years + ' years!' + ' Are you sure?';
-    document.getElementById('profit_result').innerText = 'Check the number of months, please';
-  }
 }
